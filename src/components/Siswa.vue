b01f924c0d72a23be87dc75c56290a5605741bf6
unknown <yazidmzidane@gmail.com>
2019-03-18 19:25:09 +0700

    Project

---
+++ src/components/Siswa.vue
@@ -0,0 +1,29 @@
+<template>
+ <div><br>
+  <center>
+  <table border="1">
+    <tr>
+      <td><center><b>Kelas</b></center></td>
+      <td><center><b>Nama</b></center></td>
+    </tr>
+    <tr>
+      <td>XII - RPL 1</td>
+      <td>Andriansah S</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 2</td>
+      <td>Dimas RianD</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 3</td>
+      <td>Bogalakon</td>
+    </tr>
+  </table>
+  </center>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
